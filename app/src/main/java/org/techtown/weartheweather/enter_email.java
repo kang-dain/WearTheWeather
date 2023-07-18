f619deef3e90413427ef4158c162ab86213b4af8
dain <kangdain1476@naver.com>
2023-07-19 00:54:48 +0900

    커밋

---
+++ app/src/main/java/org/techtown/weartheweather/enter_email.java
@@ -0,0 +1,14 @@
+package org.techtown.weartheweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enter_email extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_email);
+    }
+}
