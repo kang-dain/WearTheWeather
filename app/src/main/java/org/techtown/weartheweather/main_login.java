9f1a557bea8edd1e367b187ed9a305b67bee4d2b
송혜음 <parzen1@naver.com>
2023-07-18 23:45:57 +0900

    메인->로그인 화면 구현 2

--- app/src/main/java/org/techtown/weartheweather/main_login.java
+++ app/src/main/java/org/techtown/weartheweather/main_login.java
@@ -14,15 +14,24 @@ public class main_login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
 
-        ImageButton backbutton = (ImageButton) findViewById(R.id.main_login_common_backbutton);
-        backbutton.setOnClickListener(new View.OnClickListener() {
-
+        ImageButton main_backbutton = (ImageButton) findViewById(R.id.main_login_common_backbutton);
+        main_backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), main.class);
                 startActivity(intent);
             }
         });
+
+        ImageButton find_password_button = (ImageButton) findViewById(R.id.main_login_button2);
+        find_password_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), find_password.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
