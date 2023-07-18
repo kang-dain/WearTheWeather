d28d05fe00a17dd2fa4f810de0435ac2cce26755
dain <kangdain1476@naver.com>
2023-07-19 02:11:50 +0900

    커밋

---
+++ app/src/main/java/org/techtown/weartheweather/enter_password.java
@@ -0,0 +1,14 @@
+package org.techtown.weartheweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enter_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_password);
+    }
+}
