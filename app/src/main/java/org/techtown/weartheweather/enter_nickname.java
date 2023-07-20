10570747bd4094137ef9af22b7b32ba5554f8d54
yeonju000 <20221146@sungshin.ac.kr>
2023-07-20 14:20:58 +0900

    enter_nickname 액티비티, 드로어블 생성

---
+++ app/src/main/java/org/techtown/weartheweather/enter_nickname.java
@@ -0,0 +1,12 @@
+package org.techtown.weartheweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class enter_nickname extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_nickname);
+    }
+}
