ec0354b5c81a4413aef807d46c245ffa9be9ad46
yeonju000 <20221146@sungshin.ac.kr>
2023-07-20 21:30:32 +0900

    이미지 버튼 >> android.widget.Button 으로 수정

--- app/src/main/java/org/techtown/weartheweather/main_login.java
+++ app/src/main/java/org/techtown/weartheweather/main_login.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,25 +19,20 @@ public class main_login extends AppCompatActivity {
         setContentView(R.layout.activity_main_login);
 
         ImageButton main_backbutton = (ImageButton) findViewById(R.id.main_login_common_backbutton);
-        main_backbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), main.class);
-                startActivity(intent);
-            }
+        main_backbutton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), main.class);
+            startActivity(intent);
         });
 
         ImageButton find_password_button = (ImageButton) findViewById(R.id.main_login_button2);
-        find_password_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), find_password.class);
-                startActivity(intent);
-            }
+        find_password_button.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), find_password.class);
+            startActivity(intent);
         });
 
 
-        ImageButton main_login_button1 = findViewById(R.id.main_login_button1);
+        Button main_login_button1;
+        main_login_button1 = findViewById(R.id.main_login_button1);
         main_login_button1.setOnTouchListener((view, motionEvent) -> {
 
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
