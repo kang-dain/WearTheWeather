455c7a787c10fb7f22d72bac17ed5fff714a711a
yeonju000 <20221146@sungshin.ac.kr>
2023-07-27 23:28:33 +0900

    유튜브 재생 (기기에서만 가능한듯...)

--- app/src/main/java/org/techtown/weartheweather/recommended_music.java
+++ app/src/main/java/org/techtown/weartheweather/recommended_music.java
@@ -1,18 +1,45 @@
 package org.techtown.weartheweather;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.ImageButton;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-        import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class recommended_music extends AppCompatActivity {
+public class recommended_music extends YouTubeBaseActivity {
+
+    //객체 선언
+    YouTubePlayerView playerView;
+    YouTubePlayer player;
+
+    //유튜브 API KEY와 동영상 ID 변수 설정
+    private static final String API_KEY = "본인 API를 입력하세요";
+    //https://www.youtube.com/watch?v=w9TON4IwR2w ▶ 유튜브 동영상 v= 다음 부분이 videoId
+    private static final String videoId = "w9TON4IwR2w";
+
+    //logcat 사용 설정
+    private static final String TAG = "activity_recommended_music";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommended_music);
+
+        initPlayer();
+
+        Button btnPlay = findViewById(R.id.youtubeBtn);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playVideo();
+            }
+        });
 
 
         ImageButton imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
@@ -53,6 +80,63 @@ public class recommended_music extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), menu.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+    private void playVideo() {
+        if(player != null) {
+            if(player.isPlaying()) {
+                player.pause();
+            }
+            player.cueVideo(videoId);
+        }
+    }
+
+    private void initPlayer() {
+        playerView = findViewById(R.id.youTubePlayerView);
+        playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                player = youTubePlayer;
+
+                player.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+
+                    }
+
+                    @Override
+                    public void onLoaded(String id) {
+                        Log.d(TAG, "onLoaded: " + id);
+                        player.play();
+                    }
+
+                    @Override
+                    public void onAdStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoEnded() {
+
+                    }
+
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+                        Log.d(TAG, "onError: " + errorReason);
+                    }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
             }
         });
     }
