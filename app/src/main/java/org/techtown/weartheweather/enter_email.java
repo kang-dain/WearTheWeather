92522a3f5fe57d5a61810ab22a4a5629a4dc208b
yeonju000 <20221146@sungshin.ac.kr>
2023-08-05 05:15:06 +0900

    로그인 sqlite 구현

--- app/src/main/java/org/techtown/weartheweather/enter_email.java
+++ app/src/main/java/org/techtown/weartheweather/enter_email.java
@@ -3,42 +3,65 @@ package org.techtown.weartheweather;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.techtown.weartheweather.databinding.ActivityEnterEmailBinding;
+
 public class enter_email extends AppCompatActivity {
+
+    ActivityEnterEmailBinding binding;
+    DatabaseHelper databaseHelper;
+
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enter_email);
+        binding = ActivityEnterEmailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        getWindow().setWindowAnimations(0);
+        databaseHelper = new DatabaseHelper(this);
 
-        ImageButton enter_email_back_button = findViewById(R.id.enter_email_button1);
-        enter_email_back_button.setOnClickListener(new View.OnClickListener() {
+        binding.enterEmailButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), terms_of_use.class);
-                startActivity(intent);
+                String email = binding.enterEmailInput1.getText().toString();
+                String password = binding.editTextTextPassword.getText().toString();
+                String confirmPassword = binding.editTextTextPassword2.getText().toString();
+                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
+                    Toast.makeText(enter_email.this, "모든 항목을 채워주세요.", Toast.LENGTH_SHORT).show();
+                else{
+                    if(password.equals(confirmPassword)){
+                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
+                        if(checkUserEmail == false){
+                            Boolean insert = databaseHelper.insertData(email, password);
+                            if(insert == true){
+                                Toast.makeText(enter_email.this, "회원가입 성공!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),main_login.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(enter_email.this, "회원가입 실패!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(enter_email.this, "이미 존재하는 사용자입니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(enter_email.this, "암호가 잘못되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
-
-        Button enter_email_button2 = findViewById(R.id.enter_email_button2);
-        enter_email_button2.setOnTouchListener((view, motionEvent) -> {
-
-            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                enter_email_button2.setBackgroundResource(R.drawable.add_icon2_button4);
-                Intent intent = new Intent(getApplicationContext(), enter_password.class);
+        binding.enterEmailButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(enter_email.this, main_login.class);
                 startActivity(intent);
             }
-            return false;
         });
     }
 }
--- app/src/main/java/org/techtown/weartheweather/main_login.java
+++ app/src/main/java/org/techtown/weartheweather/main_login.java
@@ -3,42 +3,61 @@ package org.techtown.weartheweather;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.techtown.weartheweather.databinding.ActivityMainLoginBinding;
+
 public class main_login extends AppCompatActivity {
+
+    ActivityMainLoginBinding binding;
+    DatabaseHelper databaseHelper;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_login);
+        binding = ActivityMainLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        databaseHelper = new DatabaseHelper(this);
+
+        binding.mainLoginButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.mainemailinput.getText().toString();
+                String password = binding.mainpasswordinput.getText().toString();
+                if(email.equals("")||password.equals(""))
+                    Toast.makeText(main_login.this, "모든 항목을 채워주세요.", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+                    if(checkCredentials == true){
+                        Toast.makeText(main_login.this, "로그인 성공!", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), main_weather.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(main_login.this, "잘못된 자격증명입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        binding.mainLoginButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(main_login.this, enter_email.class);
+                startActivity(intent);
+            }
+        });
 
         ImageButton find_password_button = (ImageButton) findViewById(R.id.main_login_button2);
         find_password_button.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), find_password.class);
             startActivity(intent);
-        });
-
-        ImageButton join_button = (ImageButton) findViewById(R.id.main_login_button3);
-        join_button.setOnClickListener(view -> {
-            Intent intent = new Intent(getApplicationContext(), terms_of_use.class);
-            startActivity(intent);
-        });
-
-
-        Button main_login_button1 = findViewById(R.id.main_login_button1);
-        main_login_button1.setOnTouchListener((view, motionEvent) -> {
-
-            if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                main_login_button1.setBackgroundResource(R.drawable.add_icon1_button1);
-                Intent intent = new Intent(getApplicationContext(), main_weather.class);
-                startActivity(intent);
-            }
-            return false;
         });
     }
 }
