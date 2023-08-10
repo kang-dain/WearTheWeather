47b2c0601140d4b304fcbbfef151602e36a0622b
yeonju000 <20221146@sungshin.ac.kr>
2023-08-10 17:49:17 +0900

    날씨 api

--- app/src/main/java/org/techtown/weartheweather/main_weather.java
+++ app/src/main/java/org/techtown/weartheweather/main_weather.java
@@ -1,52 +1,71 @@
 package org.techtown.weartheweather;
 
-import static org.techtown.weartheweather.TransLocalPoint.TO_GRID;
-
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
-
 
 public class main_weather extends AppCompatActivity {
+
+    TextView dateView;
+    TextView cityView;
+    TextView weatherView;
+    TextView tempView;
+
+
+    static RequestQueue requestQueue;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_weather);
+
+        dateView = findViewById(R.id.dateView);
+        cityView = findViewById(R.id.cityView);
+        weatherView = findViewById(R.id.weatherView);
+        tempView = findViewById(R.id.tempView);
+
+
+        ImageButton button = findViewById(R.id.imageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                //시간데이터와 날씨데이터 활용
+                CurrentCall();
+            }
+        });
+
+        //volley를 쓸 때 큐가 비어있으면 새로운 큐 생성하기
+        if(requestQueue == null){
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
 
 
         ImageView main_weather_button1 = findViewById(R.id.main_weather_button1);
@@ -99,4 +118,84 @@ public class main_weather extends AppCompatActivity {
             }
         });
     }
+
+    private void CurrentCall(){
+
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=ffb0f1a2074f79704a5df7e9c27431c6";
+
+
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onResponse(String response) {
+
+                try {
+
+                    //System의 현재 시간(년,월,일,시,분,초까지)가져오고 Date로 객체화함
+                    long now = System.currentTimeMillis();
+                    Date date = new Date(now);
+
+                    //년, 월, 일 형식으로. 시,분,초 형식으로 객체화하여 String에 형식대로 넣음
+                    SimpleDateFormat simpleDateFormatDay = new SimpleDateFormat("yyyy-MM-dd");
+                    SimpleDateFormat simpleDateFormatTime = new SimpleDateFormat("HH:mm:ss");
+                    String getDay = simpleDateFormatDay.format(date);
+                    String getTime = simpleDateFormatTime.format(date);
+
+                    //getDate에 개행을 포함한 형식들을 넣은 후 dateView에 text설정
+                    String getDate = getDay + "\n" + getTime;
+                    dateView.setText(getDate);
+
+                    //api로 받은 파일 jsonobject로 새로운 객체 선언
+                    JSONObject jsonObject = new JSONObject(response);
+
+
+                    //도시 키값 받기
+                    String city = jsonObject.getString("name");
+
+                    cityView.setText(city);
+
+
+                    //날씨 키값 받기
+                    JSONArray weatherJson = jsonObject.getJSONArray("weather");
+                    JSONObject weatherObj = weatherJson.getJSONObject(0);
+
+                    String weather = weatherObj.getString("description");
+
+                    weatherView.setText(weather);
+
+
+
+                    //기온 키값 받기
+                    JSONObject tempK = new JSONObject(jsonObject.getString("main"));
+
+                    //기온 받고 켈빈 온도를 섭씨 온도로 변경
+                    double tempDo = (Math.round((tempK.getDouble("temp")-273.15)*100)/100.0);
+                    tempView.setText(tempDo +  "°C");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+
+                return params;
+            }
+
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
+    }
+
 }
