1831f85892056045c70163d0c47bc9fb44447dbb
dain <kangdain1476@naver.com>
2023-08-15 11:00:23 +0900

    검색 결과

--- app/src/main/java/org/techtown/weartheweather/search_result.java
+++ app/src/main/java/org/techtown/weartheweather/search_result.java
@@ -1,17 +1,20 @@
 package org.techtown.weartheweather;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.List;
 
+
 public class search_result extends AppCompatActivity {
+
 
     //다인
     private FeelsDataSource dataSource;
@@ -30,6 +33,29 @@ public class search_result extends AppCompatActivity {
 
         resultTextView = findViewById(R.id.resultTextView);
 
+        /**
+        // 데이터베이스에서 검색결과 가져오기
+        List<String> searchResults = dataSource.getSearchResults();
+
+        if (!searchResults.isEmpty()) {
+            String resultText = TextUtils.join("\n\n", searchResults);
+            resultTextView.setText(resultText);
+        } else {
+            resultTextView.setText("검색 결과가 없습니다.");
+        }
+    }
+
+    protected void onDestory() {
+        super.onDestory();
+
+        if(dataSource != null) {
+            dataSource.close();
+        }
+         */
+
+
+
+
         //검색조건 설정 (예. 특정 날짜와 온도에 해당하는 데이터 검색
         String searchDate = "2023-08-14";
         int searchTemperature = 25;
@@ -44,12 +70,6 @@ public class search_result extends AppCompatActivity {
         } else {
             resultTextView.setText("검색 결과가 없습니다.");
         }
-
-        dataSource.close();
-
-
-
-
 
 
         ImageButton search_result_closebutton = (ImageButton) findViewById(R.id.common_closebutton);
@@ -69,6 +89,7 @@ public class search_result extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         ImageButton imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
         imageButton6.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +98,7 @@ public class search_result extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         ImageButton imageButton7 = (ImageButton) findViewById(R.id.imageButton7);
         imageButton7.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,5 +123,10 @@ public class search_result extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        dataSource.close();
     }
 }
