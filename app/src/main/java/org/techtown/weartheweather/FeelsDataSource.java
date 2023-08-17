30940fcb8013b47b886eacf00e75445019cb7c78
dain <kangdain1476@naver.com>
2023-08-17 20:59:11 +0900

    강수량 정보 글씨 수정

--- app/src/main/java/org/techtown/weartheweather/FeelsDataSource.java
+++ app/src/main/java/org/techtown/weartheweather/FeelsDataSource.java
@@ -18,68 +18,6 @@ public class FeelsDataSource {
             dbHelper.close();
         }
 
-/**        public List<String> getSearchResults() {
-            List<String> searchResults = new ArrayList<>();
 
-            SQLiteDatabase db = dbHelper.getReadableDatabase();
-            Cursor cursor = db.query(
-                    "user_input",
-                    new String[]{"date", "temperature"},
-                    null,
-                    null,
-                    null,
-                    null,
-                    "temperature ASC"
-            );
-
-
-
-            while (cursor.moveToNext()) {
-                String date = cursor.getString(cursor.getColumnIndexOrThrow("date"));
-                int temperature = cursor.getInt(cursor.getColumnIndexOrThrow("temperature"));
-
-                String result = "Date: " + date + "\nTemperature: " + temperature;
-                searchResults.add(result);
-            }
-
-            cursor.close();
-            db.close();
-
-            return searchResults;
-    }*/
-/**
-public List<String> getSearchResults(int targetTemperature) {
-    List<String> searchResults = new ArrayList<>();
-
-    SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-    // 3도(이상, 이하) 차이를 계산하여 검색하기 위해 조건을 설정합니다
-    int minTemperature = targetTemperature - 3;
-    int maxTemperature = targetTemperature + 3;
-
-    Cursor cursor = db.query(
-            "user_input",
-            new String[]{"date", "temperature"},
-            "temperature >= ? AND temperature <= ?",
-            new String[]{String.valueOf(minTemperature), String.valueOf(maxTemperature)},
-            null,
-            null,
-            "ABS(temperature - " + targetTemperature + ") ASC, temperature ASC"
-    );
-
-    while (cursor.moveToNext()) {
-        String date = cursor.getString(cursor.getColumnIndexOrThrow("date"));
-        int temperature = cursor.getInt(cursor.getColumnIndexOrThrow("temperature"));
-
-        String result = "Date: " + date + "\nTemperature: " + temperature;
-        searchResults.add(result);
-    }
-
-    cursor.close();
-    db.close();
-
-    return searchResults;
-}
-*/
 
 }
